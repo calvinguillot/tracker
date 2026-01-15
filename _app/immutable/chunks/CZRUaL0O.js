3a91a79499b7f8882a48c4f0fa3efad860cf4990
calvinguillot <calvinguillot@gmail.com>
2026-01-15 23:22:53 +0200

    Updates

--- _app/immutable/chunks/CZRUaL0O.js
+++
@@ -1,1 +0,0 @@
-import{b as p,E as t}from"./C2GeKLD_.js";import{B as c}from"./qgtactrx.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
---
+++ _app/immutable/chunks/_vn_oups.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_bqlgvv?.base??"/tracker",a=globalThis.__sveltekit_bqlgvv?.assets??s??"";export{a,s as b};
