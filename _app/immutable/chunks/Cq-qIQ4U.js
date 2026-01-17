4253399044ec188f7ae0d717f29c56d119885135
calvinguillot <calvinguillot@gmail.com>
2026-01-17 13:17:50 +0200

    Updates

---
+++ _app/immutable/chunks/Cq-qIQ4U.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1qstuai?.base??"/tracker",a=globalThis.__sveltekit_1qstuai?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/SrGHzVK8.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_tnni1o?.base??"/tracker",t=globalThis.__sveltekit_tnni1o?.assets??s??"";export{t as a,s as b};
