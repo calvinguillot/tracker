bb73bdc35bd7620ca133a407358eef89a967b986
calvinguillot <calvinguillot@gmail.com>
2026-01-17 11:10:08 +0200

    Updates

--- _app/immutable/chunks/Bqk2hpGD.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1r5is9i?.base??"/tracker",a=globalThis.__sveltekit_1r5is9i?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/SrGHzVK8.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_tnni1o?.base??"/tracker",t=globalThis.__sveltekit_tnni1o?.assets??s??"";export{t as a,s as b};
