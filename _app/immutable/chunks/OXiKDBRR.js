e63afcc3b90c387e17b7b809ac92ed800020bccb
calvinguillot <calvinguillot@gmail.com>
2026-01-23 23:27:40 +0200

    Updates

---
+++ _app/immutable/chunks/OXiKDBRR.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_5aqwrj?.base??"/tracker",a=globalThis.__sveltekit_5aqwrj?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/bNUITWPA.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_i3081m?.base??"/tracker",a=globalThis.__sveltekit_i3081m?.assets??s??"";export{a,s as b};
