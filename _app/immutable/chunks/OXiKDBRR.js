35df914a9f1fc24e14db2bd9a31f06bc1b4ec6f7
calvinguillot <calvinguillot@gmail.com>
2026-01-24 12:16:11 +0200

    Updates

--- _app/immutable/chunks/OXiKDBRR.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_5aqwrj?.base??"/tracker",a=globalThis.__sveltekit_5aqwrj?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.zvebv8Fa.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/3Ld9vLSe.js";export{o as load_css,r as start};
