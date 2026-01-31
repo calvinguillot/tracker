fcead30e8b8aceae7bd470baea190a0c282a749c
calvinguillot <calvinguillot@gmail.com>
2026-01-31 23:26:30 +0200

    Updates

--- _app/immutable/chunks/BTArtavm.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_148aehf?.base??"/tracker",a=globalThis.__sveltekit_148aehf?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/CFuKs3JX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1s9zvi0?.base??"/tracker",a=globalThis.__sveltekit_1s9zvi0?.assets??s??"";export{a,s as b};
